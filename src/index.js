a9d5e44449a28e6ca88d7cc128982331abc83504
Marco Suarez <jmarcosuarez@gmail.com>
2017-03-03 19:49:35 +0000

    Add react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import configureStore from './stores/configureStore';
 import * as actions from './actions';
+import App from './components/App';
 import Stream from './components/Stream';
 
 const tracks = [
@@ -17,10 +20,17 @@ const tracks = [
 const store = configureStore();
 store.dispatch(actions.setTracks(tracks));
 
+const history = syncHistoryWithStore(browserHistory, store);
+
 ReactDOM.render(
- <Provider store={store}>
-   <Stream />
- </Provider>,
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Stream} />
+        <Route path="/" component={Stream} />
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('app')
 );
 
--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,7 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
+import { browserHistory } from 'react-router';
+import { routerMiddleware } from 'react-router-redux';
 import rootReducer from '../reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const router = routerMiddleware(browserHistory);
+const createStoreWithMiddleware = applyMiddleware(router)(createStore);
 
 export default function configureStore(initialState) {
   return createStoreWithMiddleware(rootReducer, initialState);
