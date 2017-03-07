dbf1a6d0bb08951a9edf70816ac4a3f591378803
Marco Suarez <jmarcosuarez@gmail.com>
2017-03-07 18:12:09 +0000

    Reduxthunk

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
 import { browserHistory } from 'react-router';
 import { routerMiddleware } from 'react-router-redux';
 import rootReducer from '../reducers';
 
+const logger = createLogger();
 const router = routerMiddleware(browserHistory);
-const createStoreWithMiddleware = applyMiddleware(router)(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk, router, logger)(createStore);
 
 export default function configureStore(initialState) {
   const store = createStoreWithMiddleware(rootReducer, initialState);
