fd8df8cd07b5af3b760ab7ac9c09e0cfaac4dc3f
Marco Suarez <jmarcosuarez@gmail.com>
2017-04-13 22:21:52 +0100

    Edit Footer

--- src/components/App/Footer/index.js
+++ src/components/App/Footer/index.js
@@ -4,8 +4,8 @@ import styles from '../App.css';
 const Footer = () =>
   <footer className={styles.info}>
     <ul>
-      <li>Search Tool Prototyping</li>
-      <li>Trips App</li>
+      <li>App description here</li>
+      <li>App Name here</li>
       <li>Code in <a href="https://github.com/jmarcosuarez/minimal-react-boilerplate">github</a></li>
     </ul>
   </footer>;
