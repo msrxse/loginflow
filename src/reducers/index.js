e02a8fb7a0399f21a84763ee5868b58b0d350bf0
Marco Suarez <jmarcosuarez@gmail.com>
2017-06-25 20:03:09 +0100

    Rename reducer register into auth reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import track from './track';
-import data from './register';
+import data from './auth';
 
 export default combineReducers({
   track,
