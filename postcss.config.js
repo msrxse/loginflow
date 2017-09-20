125b49cbe37ec386df18773d4f2343e2c63a880e
Marco Suarez <jmarcosuarez@gmail.com>
2017-09-20 09:44:38 +0100

    Implement the use of variables and mixins in postcss - Closes #29

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,10 @@
   * Tells postCSS which plugins to use
   **/
 module.exports = {
-  plugins: [
+  plugins: {
     'postcss-import': {},
-  ],
+    'postcss-cssnext': {
+      browsers: ['last 2 versions', '> 5%'], // This is for Autoprefixer, which comes built-in
+    },
+  },
 };
